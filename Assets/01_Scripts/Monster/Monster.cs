af27249afa2592eff1da4d79bb03df531d5e6744
Chaeho Yu <chaeho1234@naver.com>
2024-02-17 16:32:36 +0900

    commit

---
+++ Assets/01_Scripts/Monster/Monster.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MonsterType {
+    Penalty,
+    Benefit
+}
+
+public class Monster : MonoBehaviour
+{
+    private int hp { get; set; } = 10;
+    private int diePrameter;
+    
+    private MonsterType monsterType;
+    (int, int) monster
+    
+    //몬스터 생성될 시 타입 정하기
+
+    private void Awake()
+    {
+        monsterType = 
+    }
+}
