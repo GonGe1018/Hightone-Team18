9ea264aeecf6f69bfd50df5a93239cb067e75ab6
gunggme <ekfrifajfl2019@gmail.com>
2024-02-17 19:43:57 +0900

    Del : 초기화

--- Assets/01_Scripts/UI/UIManager.cs
+++ Assets/01_Scripts/UI/UIManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singelton<UIManager>
 {
 	[SerializeField] private AudioSource mainAudio;
 	[SerializeField] private GameObject escPanel;
@@ -39,7 +39,7 @@ public class UIManager : MonoBehaviour
 		SceneManager.LoadScene(sceneName);
 	}
 
-	public void BackToGame() // esc Á¾·á
+	public void BackToGame() // esc ï¿½ï¿½ï¿½ï¿½
 	{
 		escPanel.SetActive(false);
 		Time.timeScale = 1;
---
+++ Assets/01_Scripts/UI/UIStat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStat : Singelton<UIStat>
+{
+	public int happiness = 0;
+	public int money = 0;
+	public int honor = 0;
+}
