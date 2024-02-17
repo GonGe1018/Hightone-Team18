60b049edf388c09212ffabd587c22a0320bfe785
Chaeho Yu <chaeho1234@naver.com>
2024-02-17 18:28:25 +0900

    Monster

--- Assets/01_Scripts/Damageable.cs
+++ Assets/01_Scripts/Damageable.cs
@@ -9,19 +9,24 @@ namespace gunggme
         [SerializeField] private int _hp;
         public int HP => _hp;
 
+        public void SetHP(int hp)
+        {
+            _hp = hp;
+        }
+
         /// <summary>
         /// 데미지를 입고 죽었는지 안죽었는지 확인
         /// </summary>
         /// <param name="dmg">입는 데미지 피해</param>
         /// <returns>죽었는지 안죽었는지 확인</returns>
-        bool isDamaged(int dmg)
+        public bool isDamaged(int dmg)
         {
             _hp -= dmg;
 
             if (_hp <= 0)
             {
                 _hp = 0;
-                return false;
+                return true;
             }
             
             return false;
