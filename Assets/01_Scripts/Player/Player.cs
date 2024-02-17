6dfd4b8a2deaf410a6b27566cd5dd804b29bd561
gunggme <ekfrifajfl2019@gmail.com>
2024-02-17 17:34:46 +0900

    Feat : Create PoolManager

--- Assets/01_Scripts/Player/Player.cs
+++ Assets/01_Scripts/Player/Player.cs
@@ -1,13 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace gunggme
 {
+    [Serializable]
+    public enum PlayerState
+    {
+        infant = 0,
+        early_childhood = 1,
+        adolescence = 2,
+        youth = 3,
+        middle_age = 4,
+        old_age = 5
+    }
+    
     public class Player : MonoBehaviour
     {
         public float SpeedOfTheBall; //speed of the ball in z axis(to move forward)
         public float Damping = 0.2f; //Smoothness of the ball when moving in x axis
+
+        [Header("플레이어 상태")]
+        [SerializeField] private PlayerState _playerState;
+        [SerializeField] private GameObject[] _playerObjs;
+
+        [Header("플레이어 스탯")]
+        [SerializeField] private float _curAttackSpeed;
+        [SerializeField] private float _maxAttackSpeed;
 
         private void Update()
         {
@@ -49,9 +69,30 @@ namespace gunggme
                 }
             }
 #endif
-            
             //gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1f), SpeedOfTheBall);
             //Moving the ball forward towards z axis, Speed of the ball is needed here that how fast it will go
+        }
+
+        void ShotObj()
+        {
+            if (_curAttackSpeed > 0)
+            {
+                _curAttackSpeed -= Time.deltaTime;
+            }
+            else
+            {
+                // todo 물건 발사
+            }
+        }
+
+        public void ChangeState(PlayerState state)
+        {
+            _playerState = state;
+
+            for (int i = 0; i < _playerObjs.Length; i++)
+            {
+                _playerObjs[i].SetActive((int)_playerState == i);
+            }
         }
     }
 }
---
+++ Assets/01_Scripts/Util/PoolManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace gunggme
+{
+    public class PoolManager : Singltone<PoolManager>
+    {
+        // 프리팹들 보관할 변수
+        public GameObject[] prefab;
+    
+        // 풀 담당을 하는 리스트들
+        private List<GameObject>[] pools;
+
+        private void Awake()
+        {
+            pools = new List<GameObject>[prefab.Length];
+
+            for (int i = 0; i < pools.Length; i++)
+            {
+                pools[i] = new List<GameObject>();
+            }
+        }
+        
+        /// <summary>
+        /// 오브젝트 생성 함수
+        /// </summary>
+        /// <param name="index">배열안 오브젝트 인덱스</param>
+        /// <returns>생성하는 오브젝트</returns>
+        public GameObject Get(int index)
+        {
+            GameObject select = null;
+
+            foreach (GameObject item in pools[index])   
+            {
+                if (!item.activeSelf)
+                {
+                    select = item;
+                    select.gameObject.SetActive(true);
+                    break;
+                }
+            }
+
+            if (!select)
+            {
+                select = Instantiate(prefab[index], transform);
+                pools[index].Add(select);
+            }
+
+            return select;
+        }
+
+        /// <summary>
+        ///  현재 자식안에 있는 오브젝트들을 비활성화
+        /// </summary>
+        public void DeactivatedChileObj()
+        {
+            foreach (var pool in pools)
+            {
+                foreach (var p in pool)
+                {
+                    p.SetActive(false);
+                }
+            }
+        }
+    }
+}
